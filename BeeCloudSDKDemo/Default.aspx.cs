b1604626bad913b36c8dc55d7c8a95f5a8385eff
ZhiHao Qian <lacusrinz@gmail.com>
2016-06-21 20:38:41 +0800

    update

--- BeeCloudSDKDemo/Default.aspx.cs
+++ BeeCloudSDKDemo/Default.aspx.cs
@@ -12,9 +12,9 @@ namespace BeeCloudSDKDemo
         protected void Page_Load(object sender, EventArgs e)
         {
             //本demo账号支付宝易宝快钱无法在生产环境测试
-            BeeCloud.BeeCloud.registerApp("e66e760b-0f78-44bb-a9ae-b22729d51678", "6fb7db77-96ed-46ef-ae10-1118ee564dd3", "97ca13e4-6f40-4790-9734-ddcdc1da21db", "a1900cf2-2570-49a3-bfb8-c6e7a1bc1e21");
+            BeeCloud.BeeCloud.registerApp("c5d1cba1-5e3f-4ba0-941d-9b0a371fe719", "39a7a518-9ac8-4a9e-87bc-7885f33cf18c", "e14ae2db-608c-4f8b-b863-c8c18953eef2", "4bfdd244-574d-4bf3-b034-0c751ed34fee");
             //设置为false进入生产环境，设置为true代表测试环境
-            BeeCloud.BeeCloud.setTestMode(false);
+            BeeCloud.BeeCloud.setTestMode(true);
         }
     }
 }
